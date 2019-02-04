f22430634671c5850dd27e7f9f57069c56bbdadb
Ngrade7 <42973142+Ngrade7@users.noreply.github.com>
2019-02-04 10:18:25 -0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+// hello.c
+// compile gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello, World!\n");
+	printf("nnnnn  rrrr  gggg   \n");
+	printf("n 	n  r rr  g  gg\n");
+	printf("n   n  r  r  gggg   \n");
+	return 0;
+}

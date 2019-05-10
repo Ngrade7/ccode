86cc7fad6c045ddfd853caec9f1da86f8bc15b1d
Ngrade7 <42973142+Ngrade7@users.noreply.github.com>
2019-05-10 10:30:26 -0700

    Add files via upload

---
+++ pointers3.cpp
@@ -0,0 +1,21 @@
+//pointer3.cpp nrg
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int integers[10];
+	int * pointer;
+	pointer = integers; *integers = 11;
+	pointer++; *pointer = 22;
+	pointer = &integers[2];	*pointer = 33;
+	pointer = integers + 3; *pointer = 44;
+	pointer = integers; *(pointer+4) = 55;
+
+	for (int n = 0; n < 5; n++)
+		cout << integers[n]<<" ";
+
+		cout<<endl;
+		return 0;
+
+}

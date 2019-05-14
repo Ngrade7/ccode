b693f0c89849a80d43234410c59f0008b9d7dc81
Ngrade7 <42973142+Ngrade7@users.noreply.github.com>
2019-05-14 10:06:19 -0700

    Add files via upload

---
+++ pointers/pointers4.cpp
@@ -0,0 +1,38 @@
+// nrg pointers4.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int integers[10];
+	int * pointers;
+	pointer = integers;
+	//print array
+	cout<<"print integers[] array"<<"\n\n";
+	for (int n = 0; n < 5; n++){
+	//integers[n] = 0;
+	cout << integers[n]<<"\n";
+	}
+//Increment *pointer then assign a value
+	*pointer = 11;
+	pointer++; *pointer = 22;
+	pointer++; *pointer = 33;
+	pointer++; *pointer = 44;
+	pointer++; *pointer = 55;
+	
+	//print array
+	cout<<"\nprint &integers[] memory address "<<"\n\n";
+	for (int n = 0; n < 5; n++){
+		cout << &integers[n]<<"\n";
+		cout<<endl;
+	}
+	//print array
+	cout<<"\nprint &integers[] memory address "<<"\n\n";
+	for (int n = 0; n < 5; n++){
+		cout << integers[n]<<"\n";
+		cout<<endl;
+	}
+	
+	return 0;
+}
+	
